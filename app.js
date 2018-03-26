c72bbdc53b269fbae59556b0bfce8deb014f48e7
Horatio12345 <37767480+Horatio12345@users.noreply.github.com>
2018-03-26 06:21:47 -0400

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,13 @@ bot.on("guildMemberSpeaking", (member, bool) => {
 	if (bool) {
 	let hChannel = bot.channels.get(process.env.TCHANNEL);
 	  // hChannel.send('```css\n '+member.displayName+'```');
-	hChannel.send(' '+member.displayName+'  ');
+var d = new Date();
+var hour=d.getUTCHours().padStart(2,'0');
+var min=d.getUTCMinutes().padStart(2,'0');
+var sec=d.getUTCSeconds().padStart(2,'0');
+//[11:22:33] 
+hChannel.send('___['+hour+':'+min+':'+sec+']   '+member.displayName+'  ');
+//	hChannel.send(' '+member.displayName+'  ');		
 	}  
 
 });
