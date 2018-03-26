cc94913452e14a49d7bbb546ab5452c4d18a891b
Horatio12345 <37767480+Horatio12345@users.noreply.github.com>
2018-03-25 22:09:44 -0400

    Update app.js

--- app.js
+++ app.js
@@ -8,23 +8,26 @@ bot.on("ready", () => {
    vChannel.join()
    		.then(connection => console.log('Connected'))
   		.catch(console.error);
+  		
+  		//add ffmpeg build pack https://github.com/jayzes/heroku-buildpack-ffmpeg
 });
 
-bot.on("message", (message) => {
-	
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
-  }
-  if (message.content.startsWith("direct")) {
-	let hChannel = bot.channels.get('427355139431464981');
-  hChannel.send('hello');
-  }
-});
+//bot.on("message", (message) => {
+//	
+//  if (message.content.startsWith("ping")) {
+//    message.channel.send("pong!");
+//  }
+//  if (message.content.startsWith("direct")) {
+//	let hChannel = bot.channels.get('427355139431464981');
+//  hChannel.send('hello');
+//  }
+//});
 
 bot.on("guildMemberSpeaking", (member, bool) => { 
-	
+	if (bool) {
 	let hChannel = bot.channels.get('427355139431464981');
-  hChannel.send('voice');
+  hChannel.send(member);
+}
   
   //427355139431464981  log text channel
   //427352629379727364  General voice channel
