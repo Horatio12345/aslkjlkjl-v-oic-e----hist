b37b6bdc9b641cc3b885fbdce4ee65d015ef9009
Horatio12345 <37767480+Horatio12345@users.noreply.github.com>
2018-03-31 02:42:40 -0400

    Update app.js

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ bot.on("voiceStateUpdate", (oldMember, newMember) =>{
   let newUserChannel = newMember.voiceChannel
   let oldUserChannel = oldMember.voiceChannel
   let vlChannel = bot.channels.get(process.env.VLCHANNEL);
+  let ttsChannel = bot.channels.get(process.env.TTSCHANNEL);
 
 var d = new Date();
 var year=String(d.getUTCFullYear());
@@ -43,16 +44,19 @@ var sec=String("0"+d.getUTCSeconds()).slice(-2);
   if (oldUserChannel === undefined) {
   	//user joined channel
   	vlChannel.send('`'+year+'-'+month+'-'+day+' ['+hour+':'+min+':'+sec+'] `  '+newMember.displayName+'-'+newMember.user.username+'#'+newMember.user.discriminator+' ***JOINED*** _'+newUserChannel+'_');
+  	ttsChannel.send(newMember.displayName+' ***JOINED*** _'+newUserChannel+'_');
   }
   else {
 		if (newUserChannel === undefined) {
 			//user left channel
 			vlChannel.send('`'+year+'-'+month+'-'+day+' ['+hour+':'+min+':'+sec+'] `  '+oldMember.displayName+'-'+oldMember.user.username+'#'+oldMember.user.discriminator+' ***LEFT*** _'+oldUserChannel+'_');
+			ttsChannel.send(oldMember.displayName+' ***LEFT*** _'+oldUserChannel+'_');
 			}
 		else {
 			//user switched channel
 			if (newUserChannel!=oldUserChannel) {
 				vlChannel.send('`'+year+'-'+month+'-'+day+' ['+hour+':'+min+':'+sec+'] `  '+oldMember.displayName+'-'+oldMember.user.username+'#'+oldMember.user.discriminator+' ***SWITCHED*** _'+oldUserChannel+'_ to _'+newUserChannel+'_');
+				ttsChannel.send(oldMember.displayName+' ***SWITCHED*** _'+oldUserChannel+'_ to _'+newUserChannel+'_');
 				}
 			}
   }		
