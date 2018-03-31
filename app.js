d4d857e278febb336399d5592505d86837491378
Horatio12345 <37767480+Horatio12345@users.noreply.github.com>
2018-03-31 03:08:29 -0400

    Update app.js

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ var sec=String("0"+d.getUTCSeconds()).slice(-2);
 });
 
 bot.on("voiceStateUpdate", (oldMember, newMember) =>{
-  let newUserChannel = newMember.voiceChannel
-  let oldUserChannel = oldMember.voiceChannel
+  let newUserChannel = String(newMember.voiceChannel).substr(1)
+  let oldUserChannel = String(oldMember.voiceChannel).substr(1)
   let vlChannel = bot.channels.get(process.env.VLCHANNEL);
   let ttsChannel = bot.channels.get(process.env.TTSCHANNEL);
 
@@ -44,22 +44,23 @@ var sec=String("0"+d.getUTCSeconds()).slice(-2);
   if (oldUserChannel === undefined) {
   	//user joined channel
   	vlChannel.send('`'+year+'-'+month+'-'+day+' ['+hour+':'+min+':'+sec+'] `  '+newMember.displayName+'`'+newMember.user.username+'#'+newMember.user.discriminator+'` ***JOINED*** _'+newUserChannel+'_');
-  	ttsChannel.send(newMember.displayName+' ***JOINED*** _'+newUserChannel.substr(1)+'_');
+  	ttsChannel.send(newMember.displayName+' ***JOINED*** _'+newUserChannel+'_');
   }
   else {
 		if (newUserChannel === undefined) {
 			//user left channel
 			vlChannel.send('`'+year+'-'+month+'-'+day+' ['+hour+':'+min+':'+sec+'] `  '+oldMember.displayName+'`'+oldMember.user.username+'#'+oldMember.user.discriminator+'` ***LEFT*** _'+oldUserChannel+'_');
-			ttsChannel.send(oldMember.displayName)+' ***LEFT*** _'+oldUserChannel.substr(1)+'_');
+			ttsChannel.send(oldMember.displayName)+' ***LEFT*** _'+oldUserChannel+'_');
 			}
 		else {
 			//user switched channel
 			if (newUserChannel!=oldUserChannel) {
 				vlChannel.send('`'+year+'-'+month+'-'+day+' ['+hour+':'+min+':'+sec+'] `  '+oldMember.displayName+'`'+oldMember.user.username+'#'+oldMember.user.discriminator+'` ***SWITCHED*** _'+oldUserChannel+'_ to _'+newUserChannel+'_');
-				ttsChannel.send(oldMember.displayName+' ***SWITCHED*** _'+oldUserChannel.substr(1)+'_ to _'+newUserChannel.substr(1)+'_');
+				ttsChannel.send(oldMember.displayName+' ***SWITCHED*** _'+oldUserChannel+'_ to _'+newUserChannel+'_');
 				}
 			}
   }		
 });
+
 bot.login(process.env.TOKEN);
 //bot.login(config.token);
